a8468595a0df635f1fbbc1070efb3cb74b1e441b
GManziFS <127343298+GManziFS@users.noreply.github.com>
2025-09-24 23:30:41 -0300

    lista03

---
+++ JavaScript/lista03/ex07/js/script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('loginForm');
+    const loginInput = document.getElementById('login');
+    const senhaInput = document.getElementById('senha'); 
+    const confirmarSenhaInput = document.getElementById('confirmarSenha');
+    const limparBtn = document.getElementById('limpar');
+    const mensagemDiv = document.getElementById('mensagem');
+    
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        validarFormulario();
+    });
+    
+   
+    limparBtn.addEventListener('click', function() {
+        loginInput.value = '';
+        senhaInput.value = '';
+        confirmarSenhaInput.value = '';
+        mensagemDiv.textContent = '';
+        mensagemDiv.style.color = '';
+    });
+    
+    function validarFormulario() {
+       
+        if (loginInput.value.trim() === '') {
+            mensagemDiv.textContent = 'Erro: O campo Login deve ser preenchido.';
+            mensagemDiv.style.color = 'red';
+            return;
+        }
+
+        if (senhaInput.value.trim() === '') {
+            mensagemDiv.textContent = 'Erro: O campo de senhas deve ser preenchido.';
+            mensagemDiv.style.color = 'red';
+            return;
+        }
+        
+        
+        if (senhaInput.value !== confirmarSenhaInput.value) {
+            mensagemDiv.textContent = 'Erro: As senhas não coincidem.';
+            mensagemDiv.style.color = 'red';
+            senhaInput.value = '';
+            confirmarSenhaInput.value = '';
+            return;
+        }
+        
+        
+        mensagemDiv.textContent = 'Acesso Liberado!';
+        mensagemDiv.style.color = 'green';
+    }
+});
