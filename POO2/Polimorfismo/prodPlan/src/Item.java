5522ea1d42f2877103ce59a6c416b2f891ccc4ed
GManziFS <127343298+GManziFS@users.noreply.github.com>
2026-04-08 23:04:25 -0300

    Add files via upload

---
+++ POO2/Polimorfismo/prodPlan/src/Item.java
@@ -0,0 +1,22 @@
+public class Item {
+    Parte parte;
+    int quantidade;
+
+    public Item(Parte parte, int quantidade) {
+        this.parte = parte;
+        this.quantidade = quantidade;
+    }
+
+    public float calculaValor() {
+        return quantidade * parte.calculaValor();
+    }
+
+    @Override
+    public String toString() {
+        return "cod:" + parte.cod +
+               " nome:" + parte.nome +
+               " quantidade:" + quantidade +
+               " valor unitario:" + parte.calculaValor() +
+               " valor:" + calculaValor();
+    }
+}
